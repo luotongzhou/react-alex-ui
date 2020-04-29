ead4db23e2ff200fc01f009273604aa25b5b237a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-29 09:26:53 +0000

    Deploy luotongzhou/react-alex-ui to github.com/luotongzhou/react-alex-ui.git:gh-pages

---
+++ precache-manifest.e03e43254949b1e3882fa6da1cbef4a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3a4f2b81618e7e1e8d659b28894a2f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2161ebc599da3d94f417.bundle.js"
+  },
+  {
+    "url": "runtime~main.2161ebc599da3d94f417.bundle.js"
+  },
+  {
+    "revision": "da80e995e0a6894fa712",
+    "url": "static/css/main.7fd114cc.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.2161ebc599da3d94f417.bundle.js"
+  },
+  {
+    "url": "vendors~main.2161ebc599da3d94f417.bundle.js.LICENSE.txt"
+  }
+]);
